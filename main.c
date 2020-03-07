65528c59862be177a97617318833a30a12a65e51
crazy-historian <49170543+crazy-historian@users.noreply.github.com>
2020-03-07 11:31:27 +0300

    struct Zziper was created

--- main.c
+++ main.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+#define N 100
+
+struct Zziper
+{
+    char files [N][N];
+    char path [N];
+    char archive_name [N];
+};
+
+void Zziper__init (struct Zziper* self, char files [N][N], char path);
+void get_info(struct Zziper* self);
+void compress (struct Zziper* self);
+void decompress (struct Zziper* self);
 
 int main() {
     printf("Hello, World!\n");

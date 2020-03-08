82d13ff10f532002aa080c7b68cfa384e1e24bfe
crazy_historian <zaitsev808@mail.ru>
2020-03-08 15:09:59 +0300

    Directory traversal in Zipper structure added.

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -25,65 +24,61 @@ typedef struct Zziper
 } Zziper;
 
 void Zziper__init (Zziper* self);
-void get_info(Zziper* self);
+void list_directory(Zziper* self, string dir_name);
 void compress (Zziper* self);
 void decompress (Zziper* self);
 void repr(Zziper* self);
 
 void Zziper__init(Zziper* self)
 {
-    self->number_of_files=0;
+    self->files = (string*) malloc(sizeof(string));
+    self->number_of_files = 0;
     self->file_name = NULL;
-    self->files = NULL;
     self->path = NULL;
 }
 
-void get_info(Zziper* self)
+void list_directory(Zziper* self, string dir_name)
 {
-    string name;
-    self->files = (string*) malloc(sizeof(string));
-
+    string file_name;
     DIR *directory;
     struct dirent *dir_object;
 
-    directory = opendir(".");
-    int i = 0;
+    directory = opendir(dir_name);
+    static int i = 0;
     int len = 0;
     if (directory)
     {
         while ((dir_object = readdir(directory)) != NULL)
         {
-            name = dir_object->d_name;
-            if (strcmp(dir_object->d_name, ".") == 0 || strcmp(dir_object->d_name, "..") == 0)
-                continue;
-
             if (dir_object->d_type == DT_DIR)
             {
-                printf("%s %d\n", dir_object->d_name, dir_object->d_type);
+                if (strcmp(dir_object->d_name, ".") == 0 || strcmp(dir_object->d_name, "..") == 0)
+                    continue;
+                printf("%s %s\n", "New level of recursion:", dir_object->d_name);
+                list_directory(self, dir_object->d_name);
             }
-
-            if (dir_object->d_type == DT_DIR)
+            else
             {
-                printf("%s\n", "New level of recursion");
-            }
-            len = strlen(name) + 1;
-            self->files = (string*)realloc(self->files, sizeof(string) * (i + 2));
-            self->files[i] = (string) malloc(len);
-            snprintf(self->files[i], len,"%s", name);
-            i++;
-            self->number_of_files ++;
+                file_name = dir_object->d_name;
+                printf("%s %s\n", "Simple file: ", file_name);
+                len = strlen(file_name) + 1;
+                self->files = (string*)realloc(self->files, sizeof(string) * (i + 2));
+                self->files[i] = (string) malloc(len);
 
+                snprintf(self->files[i], len, "%s", file_name);
+                printf("%s %s\n", "Simple file: ", self->files[i]);
+                i++;
+                self->number_of_files ++;
+            }
         }
-
         closedir(directory);
-
     }
 }
 
 
 void repr (Zziper* self)
 {
-    printf("The list of scanned files", "%s\n");
+    printf("\nThe list of scanned files\n");
 
     for (int i=0; i < self->number_of_files; i++)
         printf("%s\n", self->files[i]);
@@ -117,40 +112,11 @@ void listdir(string dir_name)
 }
 int main() {
 
-//    Zziper zip;
-//    Zziper__init(&zip);
-//    get_info(&zip);
-//    repr(&zip);
-//    free(zip.files);
-
-    listdir(".");
-
-
+    Zziper zip;
+    Zziper__init(&zip);
+    list_directory(&zip, ".");
+    repr(&zip);
+    free(zip.files);
 
 
 }
-
-//void listdir(const char *dir_name, int indent)
-//{
-//    DIR *directory;
-//    struct dirent *dir_object;
-//
-//    if (!(directory = opendir(dir_name)))
-//        return;
-//
-//    while ((dir_object = readdir(directory)) != NULL) {
-//        if (dir_object->d_type == DT_DIR)
-//        {
-//            char path[1024];
-//            if (strcmp(dir_object->d_name, ".") == 0 || strcmp(dir_object->d_name, "..") == 0)
-//                continue;
-//            snprintf(path, sizeof(path), "%s/%s", dir_name, dir_object->d_name);
-//            printf("%*s[%s]\n", indent, "", dir_object->d_name);
-//            listdir(path, indent + 2);
-//        } else
-//            {
-//            printf("%*s- %s\n", indent, "", dir_object->d_name);
-//            }
-//    }
-//    closedir(directory);
-//}
